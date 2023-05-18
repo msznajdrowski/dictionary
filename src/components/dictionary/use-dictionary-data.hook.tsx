af3cc928ce5d589aeb61c5e8257c18f14d9f1828
Mateusz <mateusz@msznajdrowski-macgda.local>
2023-05-18 11:40:49 +0200

    added sessionStorage support

--- src/components/dictionary/use-dictionary-data.hook.tsx
+++ src/components/dictionary/use-dictionary-data.hook.tsx
@@ -1,11 +1,29 @@
-import { ChangeEvent, useMemo, useState } from "react";
+import { ChangeEvent, useEffect, useMemo, useState } from "react";
 import { filterStringsByPrefix } from "../../utils/filter-strings-by-prefix";
+
+const STORAGE_SEARCH_VALUE_KEY = "dictionary-search";
 
 const useDictionaryData = (data: string[]) => {
   const [searchValue, setSearchValue] = useState<string>("");
 
-  const handleChangeSearch = (event: ChangeEvent<HTMLInputElement>) =>
-    setSearchValue(event.target.value);
+  const checkForStorageValue = () => {
+    const storedValue = sessionStorage.getItem(STORAGE_SEARCH_VALUE_KEY);
+
+    if (storedValue) {
+      setSearchValue(storedValue);
+    }
+  };
+
+  const handleChangeSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+
+    setSearchValue(value);
+    sessionStorage.setItem(STORAGE_SEARCH_VALUE_KEY, value);
+  };
+
+  useEffect(() => {
+    checkForStorageValue();
+  }, [checkForStorageValue]);
 
   const filteredData = useMemo(() => {
     return filterStringsByPrefix(data, searchValue);
